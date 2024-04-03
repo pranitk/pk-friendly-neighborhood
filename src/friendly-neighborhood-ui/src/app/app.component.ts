7c3d7f82077c305d89c1b235fd2b5fd5b1228a1e
pranitk24 <pranitkulkarni24@gmail.com>
2024-04-03 16:06:48 -0400

    moved folders + some basic styling

--- src/friendly-neighborhood-ui/src/app/app.component.ts
+++ src/friendly-neighborhood-ui/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html', // todo -> replace 
   styleUrl: './app.component.scss'
 })
